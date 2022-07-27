ab4878243b92b6a1dc98030f6870b1510ce211ba
shubhangi-mobio <shubhangih.mobio@gmail.com>
2022-07-27 15:06:15 +0530

    initial

--- src/app.js
+++ src/app.js
@@ -5,7 +5,6 @@ const cors = require("cors"); //configure the web API's security
 const morgan = require("morgan"); //for log HTTP requests and errors
 // const userRouter = require("./routes/user.routes");
 const authRouter = require("./routes/auth.routes");
-// const skuRouter = require("./routes/sku.routes");
 const adminRoute = require("./routes/admin.routes");
 
 const cookieParser = require("cookie-parser");
@@ -29,11 +28,10 @@ app.use(cors());
 // v1 api routes
 // app.use("/api/user", userRouter);
 app.use('/api/auth', authRouter);
-// app.use('/api/sku',skuRouter);
 app.use('/api/admin',adminRoute);
 
 
-app.get("/home", (req, res) => {
+app.get("/", (req, res) => {
   res.json({ message: "Welcome Promo tool project 🙌" });
 });
 
