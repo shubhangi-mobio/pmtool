4fc3bbaad74932904386603f532486adea9ec71a
shubhangi-mobio <shubhangih.mobio@gmail.com>
2022-07-27 15:00:21 +0530

    new

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const port = process.env.PORT || 3003;
 
 //setup server to listen on port 3003
 
+app.get('/', (req, res) => {
+  res.send('Welcome');
+})
+
 app.listen(port, () => {
-  console.log("Server is running at " + port);
+  console.log(`Server is running at ${port}`);
 });
