0e0963fd9cb55cb1c1fcd3ff91b359f8f38f2215
shubhangi-mobio <shubhangih.mobio@gmail.com>
2022-07-27 13:09:58 +0530

    init

--- src/app.js
+++ src/app.js
@@ -3,9 +3,9 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors"); //configure the web API's security
 const morgan = require("morgan"); //for log HTTP requests and errors
-const userRouter = require("./routes/user.routes");
+// const userRouter = require("./routes/user.routes");
 const authRouter = require("./routes/auth.routes");
-const skuRouter = require("./routes/sku.routes");
+// const skuRouter = require("./routes/sku.routes");
 const adminRoute = require("./routes/admin.routes");
 
 const cookieParser = require("cookie-parser");
@@ -27,9 +27,9 @@ app.use(cookieParser());
 app.use(cors());
 
 // v1 api routes
-app.use("/api/user", userRouter);
+// app.use("/api/user", userRouter);
 app.use('/api/auth', authRouter);
-app.use('/api/sku',skuRouter);
+// app.use('/api/sku',skuRouter);
 app.use('/api/admin',adminRoute);
 
 
